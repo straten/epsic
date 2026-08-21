4151d3a0e0bb145445293e9011ec481c99a9d863
Willem van Straten <vanstraten.willem@gmail.com>
2026-08-22 00:55:34 +1200

    config/*.m4 moved to .build-commons

--- src/util/test_BoxMuller.C
+++ src/util/test_BoxMuller.C
@@ -32,19 +32,26 @@ int main ()
   for (const auto& datum : data)
     os << datum << ' ';
 
-  std::string expect = "0.519191 1.3432 0.551075 -0.402395 1.6094 -0.297697 1.52676 1.55092 0.677957 0.635312 ";
+  // the order of the pairs can be swapped
+
+  std::string expect1
+     = "0.519191 1.3432 0.551075 -0.402395 1.6094 -0.297697 1.52676 1.55092 0.677957 0.635312 ";
+  std::string expect2
+     = "1.3432 0.519191 -0.402395 0.551075 -0.297697 1.6094 1.55092 1.52676 0.635312 0.677957 ";
+
   std::string got = os.str();
  
-  if (got != expect)
+  if (got == expect1 || got == expect2)
   {
-    std::cerr << "got='" << got << "'" << std::endl;
-    std::cerr << "exp='" << expect << "'" << std::endl;
-
-    std::cerr << "BoxMuller test FAIL" << std::endl;
-    return -1;
+    std::cerr << "BoxMuller test PASS" << std::endl;
+    return 0;
   }
 
-  std::cerr << "BoxMuller test PASS" << std::endl;
-  return 0;
+  std::cerr << "got='" << got << "'" << std::endl;
+  std::cerr << "exp='" << expect1 << "'" << std::endl;
+  std::cerr << " or='" << expect2 << "'" << std::endl;
+
+  std::cerr << "BoxMuller test FAIL" << std::endl;
+  return -1;
 }
 
